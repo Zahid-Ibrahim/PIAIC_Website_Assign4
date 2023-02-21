29548efae10885b11d4ab72bb1aa3b7e33b545e6
Zahid Ibrahim <zahidibrahim.usa@gmail.com>
2023-02-21 19:45:56 +0500

    first commit

---
+++ src/app/components/database.tsx
@@ -0,0 +1,33 @@
+export const data = [
+    {
+        id: 1,
+        src:'/Zia.webp',
+        heading:'Zia Khan',
+        text:'Panaverse DAO is a community of Web 3 and Metaverse developers, designers, trainers, startup founders'
+    },
+    {
+        id: 2,
+        src:'/Zeeshan.jpg',
+        heading:'Zeeshan Hanif',
+        text:'Panaverse DAO is a community of Web 3 and Metaverse developers, designers, trainers, startup founders'
+    },
+    {
+        id: 3,
+        src:'/Daniyal.jpg',
+        heading:'Daniyal Nagori',
+        text:'Panaverse DAO is a community of Web 3 and Metaverse developers, designers, trainers, startup founders'
+    },
+    {
+        id: 4,
+        src:'/Hira.jpg',
+        heading:'Hira Khan',
+        text:'Panaverse DAO is a community of Web 3 and Metaverse developers, designers, trainers, startup founders'
+    },
+    {
+        id: 5,
+        src:'/Adil.jpg',
+        heading:'Adil Altaf',
+        text:'Panaverse DAO is a community of Web 3 and Metaverse developers, designers, trainers, startup founders'
+    }
+    
+]
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
-import './globals.css'
+'use client'
 
+import { ChakraProvider } from '@chakra-ui/react'
+import Header from './components/Header'
+import Footer from './components/Footer'
 export default function RootLayout({
   children,
 }: {
@@ -12,7 +15,13 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <ChakraProvider>
+          <Header />
+          {children}
+          <Footer />
+        </ChakraProvider>
+      </body>
     </html>
   )
 }
